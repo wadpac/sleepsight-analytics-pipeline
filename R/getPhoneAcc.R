eea58698da3ba2be9ca9b582c6ae8d88e742f5e4
Vincent van Hees <vincentvanhees@gmail.com>
2019-02-22 14:11:11 +0100

    function getPhoneAcc made less memory intensity by making it read the data in chunks

--- R/getPhoneAcc.R
+++ R/getPhoneAcc.R
@@ -8,29 +8,51 @@
 getPhoneAcc = function(filefolder, desiredtz) {
   fn_acc = dir(filefolder)
   outputMatrixAccstore = matrix(0,0,3)
+  
+  blocksize = 2000000
   for (j in 1:length(fn_acc)) {
     cat(paste0("\nLoading file ",fn_acc[j]))
-    acc = data.table::fread(file=paste0(filefolder,fn_acc[j]),sep="\t")
-    acc = as.data.frame(acc)
-    acc = replaceVarWithSpace(acc)
-    NR = nrow(acc)
-    acc$Created.Date.POSIX = as.POSIXlt((acc$Normalized.Timestamp)/(1e+9),tz=desiredtz,origin="1970-1-1")
-    acc = acc[order(acc$Created.Date.POSIX),]
-    # calculate indicator of movement (sometimes referred to as ENMO):
-    acc$acceleration = pmax(0,sqrt(acc$X^2+acc$Y^2+acc$Z^2)-9.81)
-    acc$dt = 0
-    acc$dt[2:NR] = diff(as.numeric(acc$Created.Date.POSIX))
-    acc$dt[1] = acc$dt[2] # dt = deltatime
-    acc$sf = 1/acc$dt
-    acc = acc[which(acc$sf != Inf & acc$sf != 0 & acc$sf < 200),]
-    timer0 = Sys.time() # keep track of how long it taks to process the file
-    acc$num_time = as.numeric(acc$Created.Date.POSIX) # work with numeric time
-    acc$num_time_5sec = round(acc$num_time / 5) * 5 # round time to nearest 5 seconds
-    # aggregate per 5 seconds:
-    acc.per5sec = aggregate(x = acc[c("acceleration","sf")],
-                            FUN = mean, by = list(Group.time = acc$num_time_5sec),
-                            na.rm=TRUE, na.action=NULL)
-    outputMatrixAccstore = rbind(outputMatrixAccstore,acc.per5sec)
+    endlastblock = 0
+    acc=c()
+    stopprocess = FALSE
+    fname = paste0(filefolder,fn_acc[j])
+    varnames = colnames(data.table::fread(file=fname,sep="\t",nrows = 3))
+    blocki = 1
+    while(stopprocess == FALSE) { # while loop iterates over the file
+      try(expr={acc = data.table::fread(file=fname,sep="\t",nrows = blocksize, skip = endlastblock)},silent=TRUE)
+      if (length(acc) == 0) {
+        stopprocess = TRUE
+      } else {
+        # cat(" ",length(acc)," ")
+        # cat(" ",paste0(nrow(acc)," ",endlastblock," "))
+        colnames(acc) = varnames
+        cat(".")
+        endlastblock = blocki * blocksize
+        blocki = blocki + 1
+        acc = as.data.frame(acc)
+        acc = replaceVarWithSpace(acc)
+        NR = nrow(acc)
+        acc$Created.Date.POSIX = as.POSIXlt((acc$Normalized.Timestamp)/(1e+9),tz=desiredtz,origin="1970-1-1")
+        acc = acc[order(acc$Created.Date.POSIX),]
+        # calculate indicator of movement (sometimes referred to as ENMO):
+        acc$acceleration = pmax(0,sqrt(acc$X^2+acc$Y^2+acc$Z^2)-9.81)
+        acc$dt = 0
+        acc$dt[2:NR] = diff(as.numeric(acc$Created.Date.POSIX))
+        acc$dt[1] = acc$dt[2] # dt = deltatime
+        acc$sf = 1/acc$dt
+        acc = acc[which(acc$sf != Inf & acc$sf != 0 & acc$sf < 200),]
+        timer0 = Sys.time() # keep track of how long it taks to process the file
+        acc$num_time = as.numeric(acc$Created.Date.POSIX) # work with numeric time
+        acc$num_time_60sec = round(acc$num_time / 60) * 60 # round time to nearest 5 seconds
+        # aggregate per 60 seconds:
+        acc.per60sec = aggregate(x = acc[c("acceleration","sf")],
+                                FUN = mean, by = list(Group.time = acc$num_time_60sec),
+                                na.rm=TRUE, na.action=NULL)
+        outputMatrixAccstore = rbind(outputMatrixAccstore,acc.per60sec)
+        if (NR < blocksize) stopprocess = TRUE
+        rm(acc)
+      }
+    }
   }
   outputMatrixAcc = outputMatrixAccstore
   acc = data.frame(Created.Date.POSIX = as.POSIXlt(outputMatrixAcc[,1],origin = "1970-1-1",tz=desiredtz),
--- exploredata.R
+++ exploredata.R
@@ -20,40 +20,45 @@ desiredtz = "Europe/London"
 # Declare common functions:
 library(sleepsanpl)
 library(data.table)
-library(bit64)
+
+timer0 = Sys.time()
 #-----------------------------------------
-# extract battery information
+print("extract battery information")
 batfolder = "pdk-device-battery/"
 fn_bat = dir(batfolder)
 filename = paste0(batfolder,fn_bat)
-batInteractTimes = getBatInteract(filename) # timestamps when phone was either put on charged or plugged out.
-
+batInteractTimes = getBatInteract(filename, desiredtz) # timestamps when phone was either put on charged or plugged out.
+timer1 = Sys.time()
+print(timer1-timer0)
 #-------------------------------------
-# pdk-foreground-application
+print("pdk-foreground-application")
 appfolder = "pdk-foreground-application/"
 fn_app = dir(appfolder)
 filename = paste0(appfolder,fn_app)
-AppActiveTimes = getAppActive(filename)
-
+AppActiveTimes = getAppActive(filename, desiredtz)
+timer2 = Sys.time()
+print(timer2-timer1)
 #-------------------------------------
-# pdk-location
+print("pdk-location")
 locfolder = "pdk-location/"
 fn_loc = dir(locfolder)
 filename = paste0(locfolder,fn_loc)
-MovementPSGTimes = getMovementPSG(filename)
-
-# # -------------------------------------
-# # pdk-screen-state
-# # Note: probably not relevant, because screen activity does not necessarily
-# # say much about whether the person interacted with the phone, e.g. incoming call
-# # or messages...?
+MovementPSGTimes = getMovementPSG(filename, desiredtz)
+timer3 = Sys.time()
+print(timer3-timer2)
+# -------------------------------------
+print("pdk-screen-state")
+# Note: probably not relevant, because screen activity does not necessarily
+# say much about whether the person interacted with the phone, e.g. incoming call
+# or messages...?
 screstafolder = "pdk-screen-state/"
 fn_scresta = dir(screstafolder)
 filename = paste0(screstafolder,fn_scresta)
-ScreenOnTimes = getScreenState(filename)
-
+ScreenOnTimes = getScreenState(filename, desiredtz)
+timer4 = Sys.time()
+# print(timer4-timer3)
 #-------------------------------------
-# pdk-sensor-accelerometer
+print("pdk-sensor-accelerometer")
 accfolder = "pdk-sensor-accelerometer/"
 PhoneAcc = getPhoneAcc(accfolder, desiredtz) # Note that this function provides all acceleration information
 # Comments on usefulness:
@@ -62,20 +67,23 @@ PhoneAcc = getPhoneAcc(accfolder, desiredtz) # Note that this function provides 
 # - We cannot use the default time extraction because that would only reflect when data
 #   blocks are created. Instread used "Normalized Timestamp"
 # - It seems that timestamps are not ordered correctly, so first order timestamps.
-
+timer5 = Sys.time()
+print(timer5-timer4)
 #-------------------------------------
-# pdk-sensor-light
+print("pdk-sensor-light")
 # Light probably not useful, because light can change without the person change activity state
 filefolder = "pdk-sensor-light/"
 lightOnTimes = getLight(filefolder, desiredtz)
-
-
-##-------------------------------------
-# Withings device
+timer6 = Sys.time()
+print(timer6-timer5)
+#-------------------------------------
+print("Withings device")
 filefolder = "Withings-20190215T093727Z-001/" # note that function searches this folder recursevely for files that meet the description
-withingsdata = getWithingsData(filefolder)
-
-
+withingsdata = getWithingsData(filefolder, desiredtz)
+timer7 = Sys.time()
+print(timer7-timer6)
+print("Total")
+print(timer7-timer0)
 #======================================
 # Information not used:
 #======================================
