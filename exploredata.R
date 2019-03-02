122f3b1168759fd807c4ed23913bc731505e9f48
Vincent van Hees <vincentvanhees@gmail.com>
2019-03-02 12:47:47 +0100

    fixed error with plot of sunrise/set

--- exploredata.R
+++ exploredata.R
@@ -16,15 +16,20 @@ studyfolder = "/media/vincent/sleepsight"
 
 #==============================================================
 foldersInStudyFolder = list.dirs(studyfolder, recursive=FALSE)
-plothist = function(namevar,CDF,df,plottitle) {
-  if (namevar %in% CDF)  {
-    hist(df$min_inday[which(df[,namevar]==TRUE)], axes = FALSE, xlab="",ylab="",main = "")
-    axis(side = 1,at = Xposi,labels = Xlabe); title(main=plottitle,xlab="time")
-  }
-}
+
 Xlabe= c("00:00","06:00","12:00","18:00","24:00") # x label for plots
 Xposi = c(0,6,12,18,24) * 60 # x positions for plots
-CX = 0.05 # cex for plotting
+
+plothist = function(namevar="",CDF,x="",plottitle="", xpos=Xposi, xlabe=Xlabe) {
+  if (namevar %in% CDF)  {
+    if (namevar == "SunSetRise") {
+      hist(x$min_inday[which(is.na(x$SunSetRise) == FALSE)], axes = FALSE,xlab="",ylab="",main = "")
+    } else {
+      hist(x$min_inday[which(x[,namevar]==TRUE)], axes = FALSE, xlab="",ylab="",main = "")
+    }
+    axis(side = 1,at = xpos,labels = xlabe); title(main=plottitle,xlab="time")
+  }
+}
 for (personfolder in foldersInStudyFolder) {
   timer0 = Sys.time()
   cat("\n==================================================================================")
@@ -41,32 +46,22 @@ for (personfolder in foldersInStudyFolder) {
   
   cat("\n* Create histograms")
   if (do.plot == TRUE) { # simple historgram of all available data channels within a person
-    RDAfiles = dir(outputfolder,full.names = TRUE)
-    for (RDAfile in RDAfiles) {
-      if (length(unlist(strsplit(RDAfile,"[.]cs"))) < 2 & length(unlist(strsplit(RDAfile,"[.]pn"))) < 2) {
-        load(file=RDAfile)
-      }
-    }
-    
     df = read.csv(file=paste0(outputfolder,"/",csvfile))
-
+    CDF = colnames(df)
     png(filename = paste0(outputfolder,"/histograms_test.png"),width = 12,height = 10,units = "in",res = 400)
     par(mfrow=c(3,4))
-    
-    CDF = colnames(df)
-
-    plothist(namevar="screenon",CDF=CDF,df=df,plottitle="Phone screen is on")
-    plothist(namevar="lighton",CDF=CDF,df=df,plottitle="Phone light level above 10")
-    plothist(namevar="PSGmove",CDF=CDF,df=df,plottitle="Phone PSG/Speed indicate movement")
-    plothist(namevar="AppAct",CDF=CDF,df=df,plottitle="Phone foreground app is on")
-    plothist(namevar="batinteract",CDF=CDF,df=df,plottitle="Phone battery put on/off charge")
-    plothist(namevar="phoneacc" ,CDF=CDF,df=df,plottitle="Phone accelerates > 0.03 times gravity")
-    plothist(namevar="withingsMove" ,CDF=CDF,df=df,plottitle="Withings moves / Body info entered")
-    plothist(namevar="SunSetRise" ,CDF=CDF,df=df,plottitle="sunset or sunrise")
-    plothist(namevar="AppHalted" ,CDF=CDF,df=df,plottitle="AppHalted (restarted)")
-    plothist(namevar="lightsleep" ,CDF=CDF,df=df,plottitle="Withings LightSleep")
-    plothist(namevar="deepsleep" ,CDF=CDF,df=df,plottitle="Withings deep-sleep")
-    plothist(namevar="awake" ,CDF=CDF,df=df,plottitle="Withings awake")
+    plothist(namevar="screenon",CDF=CDF,x=df,plottitle="Phone screen is on")
+    plothist(namevar="lighton",CDF=CDF,x=df,plottitle="Phone light level above 10")
+    plothist(namevar="PSGmove",CDF=CDF,x=df,plottitle="Phone PSG/Speed indicate movement")
+    plothist(namevar="AppAct",CDF=CDF,x=df,plottitle="Phone foreground app is on")
+    plothist(namevar="batinteract",CDF=CDF,x=df,plottitle="Phone battery put on/off charge")
+    plothist(namevar="phoneacc" ,CDF=CDF,x=df,plottitle="Phone accelerates > 0.03 times gravity")
+    plothist(namevar="withingsMove" ,CDF=CDF,x=df,plottitle="Withings moves / Body info entered")
+    plothist(namevar="SunSetRise" ,CDF=CDF,x=df,plottitle="sunset or sunrise")
+    plothist(namevar="AppHalted" ,CDF=CDF,x=df,plottitle="AppHalted (restarted)")
+    plothist(namevar="lightsleep" ,CDF=CDF,x=df,plottitle="Withings LightSleep")
+    plothist(namevar="deepsleep" ,CDF=CDF,x=df,plottitle="Withings deep-sleep")
+    plothist(namevar="awake" ,CDF=CDF,x=df,plottitle="Withings awake")
     dev.off()
   }
   timer1 = Sys.time()
