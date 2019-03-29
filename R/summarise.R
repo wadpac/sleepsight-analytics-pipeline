68719d72c631f05b0eb0799b0bedfc93414741df
Vincent van Hees <vincentvanhees@gmail.com>
2019-03-29 18:19:59 +0100

    various fixes in the aggregation of the data

--- R/summarise.R
+++ R/summarise.R
@@ -6,6 +6,7 @@
 #' @return List with two data frames: D24HR and D5min
 #' @export
 summarise = function(outputfolder, csvfile, desiredtz) {
+  D24HR = D5min = Dsurvey = c()
   if (file.exists(paste0(outputfolder,"/",csvfile))) {
     D = data.table::fread(paste0(outputfolder,"/",csvfile))
     D = as.data.frame(D)
@@ -49,22 +50,31 @@ summarise = function(outputfolder, csvfile, desiredtz) {
                         is.na(D$batinteract == TRUE) & is.na(D$phoneacc) == TRUE &
                         is.na(D$AppHalted) == TRUE & is.na(D$withingsactive) == TRUE & is.na(D$withingsleep) == TRUE)
       D$status[missing] = 4 # if no data is available from any channels then label as missing
-      
       # Create new dataframe with only status and timestamps
       tmpmin = D[,c("time","status")]
       
-      # Create continuous timeseries
+      surveyfilename = paste0(outputfolder,"/SleepSurvey.RData")
+      if (file.exists(surveyfilename) == TRUE) {
+        load(surveyfilename)
+        Dsurvey = SleepSurvey[,c("positiveFeelings","negativeFeelings","Sleep.Quality.Value","surveytime")]
+        Dsurvey$date = as.Date(as.POSIXlt(Dsurvey$surveytime,tz=desiredtz))
+      }
+      # Untill here the data may include gaps in time.
+      # We will now create continuous time series to ease plotting
       time.POSIX = as.POSIXlt(tmpmin$time,tz=desiredtz)
       tmpmin$time.POSIX = time.POSIX
-      
       tmpmin$time_num = as.numeric(tmpmin$time.POSIX)
       complete_time = seq(min(tmpmin$time_num),max(tmpmin$time_num),by=60)
       D_complete_time = data.frame(time_num=complete_time)
-      Dminute = merge(D_complete_time,tmpmin,by="time_num")
+      tmpmin = tmpmin[,-c(which(colnames(tmpmin) %in% c("time","time.POSIX") == TRUE))]
+      Dminute = base::merge(D_complete_time,tmpmin,by="time_num",all = TRUE)
       missingstatus = which(is.na(Dminute$status) == TRUE) # missing or inconclusive
-      if (length(missingstatus) > 0) D$status[missingstatus] = 4
+      if (length(missingstatus) > 0) Dminute$status[missingstatus] = 4
+      Dminute$time = as.POSIXlt(Dminute$time_num,origin="1970-1-1",tz=desiredtz)
       Dminute$date = as.Date(Dminute$time)
-      # Aggregate per day to indentify 
+      #========================================================
+      # Remove days with not enough data
+      # To do this we have to aggregate per day
       tmpday_active = aggregate(x = Dminute[,"status"],by = list(date = Dminute$date),FUN = function(x) length(which(x==1)))
       colnames(tmpday_active) = c("date", "active")
       tmpday_inactive = aggregate(x = Dminute[,"status"],by = list(date = Dminute$date),FUN = function(x) length(which(x==0)))
@@ -76,81 +86,113 @@ summarise = function(outputfolder, csvfile, desiredtz) {
       Dday = merge(tmpday_active,tmpday_inactive,by="date")
       Dday = merge(Dday,tmpday_sleep,by="date")
       Dday = merge(Dday,tmpday_missing,by="date")
-      # Exclude days with more than 80% missing or inconclusive data or less than X minutes of active and sleep data
-      daysexclude = which(Dday$active < 10 | Dday$sleep < 10 | Dday$missing > (1440*(1/3)) )
+      # Exclude days with more than 80% missing or
+      #  or inconclusive data
+      #  or less than X minutes of active and sleep data
+      #  or less than 1440 minutes in a day
+      DAYLENGTH = aggregate(x = Dminute$status,by = list(date = Dminute$date),FUN = function(x) length(x))
+      shortdays = Dminute$date[which(Dminute$date %in% DAYLENGTH$date[which(DAYLENGTH$x < 1440)] ==  TRUE)]
+      daysexclude = which(Dday$active < 10 | Dday$sleep < 10 | Dday$missing > (1440*(1/3)))
+      if (length(shortdays) > 0) { # exclude also days with less 
+        daysexclude = unique(c(daysexclude,which(Dday$date %in% shortdays == TRUE)))
+      }
       if (length(daysexclude) > 0) {
         Dminute = Dminute[-which(Dminute$date %in% Dday$date[daysexclude] == TRUE),]
         Dday = Dday[-daysexclude,]
       }
-      # Go back to Dminute (1 minute resolution data)
-      # if status is missing then take most common value of either inactive or sleep (active criteria are not met) in 90 minute window
-      D9 = Dminute$status
-      is.na(D9[which(D9 %in% c(1,4))]) = TRUE
-      D8 = zoo::rollapply(D9,FUN = function(x) mean(x, na.rm=TRUE), width = 90) # most common value
-      index_missing = which(Dminute$status == 4)
-      Dminute$status[index_missing] = round(D8[index_missing]) # replace missing values by most common value in window
+      
+      
+      
       # turn value into sleep if 95% or more of 90 minutes is inactive
       D6 = zoo::rollapply(Dminute$status,FUN=function(x) length(which(x == 0)),width = 90)
-      Dminute$status[which(D6 > (90*0.95))] = -1 
-      #  Aggregate per 5 minutes (most common status is used)
-      Dminute$time_num_5min = round(Dminute$time_num / 5) * 5 # time rounded to five minutes
+      Dminute$status[which(D6 > (90*0.95))] = -1
+      
+      # TO DO: This could be a good place to impute missing values, if we want to impute values
+      #========================================================
+      # Aggregate per 5 minutes (the most common status is used)
+      Dminute$time_num_5min = round(Dminute$time_num / 300) * 300 # time rounded to five minutes (300 seconds)
       if (nrow(Dminute) > 1440) {
-        
-        D5min = aggregate(x = Dminute$status,by = list(time = Dminute$time_num_5min),FUN = function(x) {
+        calcmode = function(x) {
           MM = pracma::Mode(x)
           if (length(MM) == 0) MM = NA
           return(MM)
-        })
+        }
+        D5min = aggregate(x = Dminute$status,by = list(time = Dminute$time_num_5min),FUN = calcmode)
         colnames(D5min) = c("time", "status")
-        
         D5min$time.POSIX = as.POSIXlt(D5min$time,tz=desiredtz,origin="1970-01-01")
         D5min$date = as.character(as.Date(D5min$time.POSIX))
         D5min$hour = D5min$time.POSIX$hour
         D5min$min = D5min$time.POSIX$min
         D5min$hour_in_day = D5min$hour + (D5min$min/60)
         D5minB = D5min
-        # Estimate time spent in status classes per day
+        #========================================================
+        # Aggregate per day
+        mydivfun = function(tmp,dn) {
+          # divide tmp by dn while ignore NA values
+          nax = which(is.na(tmp$x) == FALSE); tmp$x[nax] = tmp$x[nax] / dn 
+          return(tmp)
+        }
         sleepdur_perday = aggregate(x = D5minB$status,by = list(date = D5minB$date),FUN = function(x) length(which(x==-1)))
-        colnames(sleepdur_perday) = c("date","sleepdur")
+        sleepdur_perday = mydivfun(sleepdur_perday,dn=12) # convert x-5-minute window of a day into hour
+        colnames(sleepdur_perday) = c("date","sleepdur") # duration expressed in minutes
+        
         activedur_perday = aggregate(x = D5minB$status,by = list(date = D5minB$date),FUN = function(x) length(which(x==1)))
+        activedur_perday = mydivfun(activedur_perday,dn=12)
         colnames(activedur_perday) = c("date","activedur")
+        
         inactivedur_perday = aggregate(x = D5minB$status,by = list(date = D5minB$date),FUN = function(x) length(which(x==0)))
+        inactivedur_perday = mydivfun(inactivedur_perday,dn=12)
         colnames(inactivedur_perday) = c("date","inactivedur")
+        
+        missingdur_perday = aggregate(x = D5minB$status,by = list(date = D5minB$date),FUN = function(x) length(which(x==4)))
+        missingdur_perday = mydivfun(missingdur_perday,dn=12)
+        colnames(missingdur_perday) = c("date","missingdur")
+        
         D24HR = merge(sleepdur_perday,activedur_perday,by="date")
         D24HR = merge(D24HR,inactivedur_perday,by="date")
+        D24HR = merge(D24HR,missingdur_perday,by="date")
         # simplify classes to be able to do L5M10 analysis
         D5minB$status[which(D5minB$status == -1)] = 0 #sleep becomes inactivity
-        # Calculate basic active/inactive descriptives per day:
+        is.na(D5minB$status[which(D5minB$status == 4)]) = TRUE #missing data becomes NA
+        # Calculate other summary statistics:
+        #--------------------------------------------------------
         D5minB$rollmean5HR = zoo::rollapply(D5minB$status,FUN=function(x) mean(x,na.rm=TRUE),width=(5*12),fill=NA)
-        D5minB$rollmean10HR = zoo::rollapply(D5minB$status,FUN=function(x) mean(x,na.rm=TRUE),width=(10*12),fill=NA)
-        tmp = aggregate(x = D5minB$rollmean5HR,by = list(date = D5minB$date),FUN = function(x) which.min(x))
-        tmp$x = tmp$x / 12
+        tmp = aggregate(x = D5minB$rollmean5HR,by = list(date = D5minB$date),FUN = function(x) which.min(x)[1])
+        tmp = mydivfun(tmp,dn=12) # convert x-5-minute window of a day into hour
         colnames(tmp) = c("date","L5hour")
         D24HR = merge(D24HR,tmp,by="date")
-        
-        tmp = aggregate(x = D5minB$rollmean5HR,by = list(date = D5minB$date),FUN = function(x) min(x))
-        tmp$x = tmp$x / 12
+        tmp = aggregate(x = D5minB$rollmean5HR,by = list(date = D5minB$date),FUN = function(x) {
+          mv = min(x,na.rm = TRUE)
+          if (mv == Inf | mv == Inf) mv = NA
+          return(mv)
+        })
         colnames(tmp) = c("date","L5value")
         D24HR = merge(D24HR,tmp,by="date")
-        tmp = aggregate(x = D5minB$rollmean10HR,by = list(date = D5minB$date),FUN = function(x) which.max(x))
-        tmp$x = tmp$x / 12
+        #--------------------------------------------------------
+        D5minB$rollmean10HR = zoo::rollapply(D5minB$status,FUN=function(x) mean(x,na.rm=TRUE),width=(10*12),fill=NA)
+        tmp = aggregate(x = D5minB$rollmean10HR,by = list(date = D5minB$date),FUN = function(x) which.max(x)[1])
+        tmp = mydivfun(tmp,dn=12)
         colnames(tmp) = c("date","M10hour")
         D24HR = merge(D24HR,tmp,by="date")
-        tmp = aggregate(x = D5minB$rollmean10HR,by = list(date = D5minB$date),FUN = function(x) max(x))
-        tmp$x = tmp$x / 12
+        tmp = aggregate(x = D5minB$rollmean10HR,by = list(date = D5minB$date),FUN = function(x) {
+          mv = max(x,na.rm = TRUE)
+          if (mv == Inf | mv == Inf) mv = NA
+          return(mv)
+        })
         colnames(tmp) = c("date","M10value")
         D24HR = merge(D24HR,tmp,by="date")
+        #--------------------------------------------------------
+        tmp = aggregate(x = D5minB$status,by = list(date = D5minB$date),FUN = function(x) length(x))
+        tmp = mydivfun(tmp,dn=12)
+        colnames(tmp) = c("date","daylength")
+        D24HR = merge(D24HR,tmp,by="date")
+        #--------------------------------------------------------
         tmp = aggregate(x = D5minB$status,by = list(date = D5minB$date),FUN = function(x) mean(x,na.rm = TRUE))
         colnames(tmp) = c("date","mean")
         D24HR = merge(D24HR,tmp,by="date")
-      } else {
-        D24HR=c()
-        D5min=c()
       }
-      D24HR=c()
-      D5min=c()
     }
-    return(list(D24HR=D24HR,D5min=D5min))
+    return(list(D24HR=D24HR,D5min=D5min,Dsurvey=Dsurvey))
     
   }
 }
